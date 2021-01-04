8a1c769cc4e3c585b0ecd91c19a8da217e6bdb47
Vynkode <kevinmuqu@me.com>
2021-01-04 13:39:04 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
-import register from './registerServiceWorker';
+import unregister from './registerServiceWorker';
 import 'tachyons';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-register();
+unregister();
